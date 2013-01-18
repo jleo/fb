017f42cf2b7077082275f0adde9f5ae60a7d6c9f
JLeo <ggyyleo@gmail.com>
2013-01-19 00:31:34 +0800

    fix

--- football/src/main/groovy/org/BatchRun.groovy
+++ football/src/main/groovy/org/BatchRun.groovy
@@ -17,18 +17,37 @@ class BatchRun {
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(Integer.parseInt(Props.getProperty("thread")));
 
-        def date = Date.parse("yyyy-MM-dd", "2013-01-01")
-        17.times {
-            BetMatchBatchProcessorSpecifiedDate betMatchBatchProcessor = new BetMatchBatchProcessorSpecifiedDate(executorService, date.format("yyyy-MM-dd"), (date + 1).format("yyyy-MM-dd"));
+        double seedExpectation = 0.0;
+        double seedProbability = 0.5;
+        int loopingExpectation = 20;
+        int loppingProbability = 10;
 
-            double minExpectation = Double.parseDouble(Props.getProperty("minExpectation"));//0.03;
-            double minProbability = Double.parseDouble(Props.getProperty("minProbability"));//0.58;
-            betMatchBatchProcessor.betBatchMatchHandicapGuarantee(minExpectation, minProbability);
+        for (int i = 1; i < loopingExpectation; ++i) {
+            for (int j = 0; j < loppingProbability; ++j) {
+                System.out.println("trying seedExpectation:" + seedExpectation + ", " + "seedProbability:" + seedProbability);
 
-            Settle s = new Settle()
-            println date.format("yyyy-MM-dd")
-            s.settle()
-            date = date + 1
+
+                def date = Date.parse("yyyy-MM-dd", "2013-01-01")
+                17.times {
+                    BetMatchBatchProcessorSpecifiedDate betMatchBatchProcessor = new BetMatchBatchProcessorSpecifiedDate(executorService, date.format("yyyy-MM-dd"), (date + 1).format("yyyy-MM-dd"));
+
+//                    double minExpectation = Double.parseDouble(Props.getProperty("minExpectation"));//0.03;
+//                    double minProbability = Double.parseDouble(Props.getProperty("minProbability"));//0.58;
+
+                    betMatchBatchProcessor.betBatchMatchHandicapGuarantee(seedExpectation, seedProbability);
+
+                    Settle s = new Settle()
+                    println date.format("yyyy-MM-dd")
+                    s.settle()
+                    date = date + 1
+                }
+
+                seedProbability = seedProbability + j * 0.02;
+            }
+            seedExpectation = seedExpectation + i * 0.005;
+            seedProbability = 0.0;
         }
+
+
     }
 }
