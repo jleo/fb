55e94228f09a18c5e484957dea272c25bcea371b
JLeo <ggyyleo@gmail.com>
2013-01-18 20:00:35 +0800

    add date

--- Laputa/src/main/java/Util/MongoDBUtil.java
+++ Laputa/src/main/java/Util/MongoDBUtil.java
@@ -32,8 +32,8 @@ public class MongoDBUtil {
 
         try {
             MongoOptions options = new MongoOptions();
-            options.setThreadsAllowedToBlockForConnectionMultiplier(30);
-            options.setConnectionsPerHost(300);
+            options.threadsAllowedToBlockForConnectionMultiplier = 30;
+            options.connectionsPerHost = 300;
             mongo = new Mongo(mongoDBHost, Integer.parseInt(mongoDBPort));
         } catch (UnknownHostException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
