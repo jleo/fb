841e7a1cca86b544a7a1e3ac79b5b9753a10f6f2
JLeo <ggyyleo@gmail.com>
2013-01-18 18:30:10 +0800

    add date

--- Laputa/src/main/java/Util/MongoDBUtil.java
+++ Laputa/src/main/java/Util/MongoDBUtil.java
@@ -22,7 +22,7 @@ public class MongoDBUtil {
 
     private boolean isConnected;
 
-    public MongoDBUtil(String MongoDBHost, String MongoDBPort, String MongoDBName){
+    public MongoDBUtil(String MongoDBHost, String MongoDBPort, String MongoDBName) {
         this.mongoDBHost = MongoDBHost;
         this.mongoDBPort = MongoDBPort;
         this.mongoDBName = MongoDBName;
@@ -54,109 +54,111 @@ public class MongoDBUtil {
         this.mongoDBName = mongoDBName;
     }
 
-    public void getConnection(){
-        closeConnection();
+    public void getConnection() {
+//        closeConnection();
         try {
-            mongo = new Mongo(mongoDBHost, Integer.parseInt(mongoDBPort));
+            if (mongo == null)
+                mongo = new Mongo(mongoDBHost, Integer.parseInt(mongoDBPort));
+
             mongoDB = mongo.getDB(mongoDBName);
             isConnected = true;
-        } catch (Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    public void closeConnection(){
-        if (isConnected){
-            mongo.close();
-        }
+    public void closeConnection() {
+//        if (isConnected) {
+//            mongo.close();
+//        }
     }
 
 
-    public void insert(DBObject object, String collectionName){
-        if (!isConnected){
+    public void insert(DBObject object, String collectionName) {
+        if (!isConnected) {
             getConnection();
         }
         DBCollection collection = mongoDB.getCollection(collectionName);
         collection.insert(object);
     }
 
-    public DBObject findOne(DBObject query, String collectionName){
-        if (!isConnected){
+    public DBObject findOne(DBObject query, String collectionName) {
+        if (!isConnected) {
             getConnection();
         }
         DBCollection collection = mongoDB.getCollection(collectionName);
         return collection.findOne(query);
     }
 
-    public DBObject findOne(DBObject query, DBObject field, String collectionName){
-        if (!isConnected){
+    public DBObject findOne(DBObject query, DBObject field, String collectionName) {
+        if (!isConnected) {
             getConnection();
         }
         DBCollection collection = mongoDB.getCollection(collectionName);
         return collection.findOne(query, field);
     }
 
-    public List<DBObject> findAll(DBObject query, String collectionName){
-        if (!isConnected){
+    public List<DBObject> findAll(DBObject query, String collectionName) {
+        if (!isConnected) {
             getConnection();
         }
         List<DBObject> resultList = new ArrayList<DBObject>();
         DBCollection collection = mongoDB.getCollection(collectionName);
         DBCursor cursor = collection.find(query);
-        while (cursor.hasNext()){
+        while (cursor.hasNext()) {
             DBObject dbObject = cursor.next();
             resultList.add(dbObject);
         }
         return resultList;
     }
 
-    public List<DBObject> findAll(DBObject query, DBObject filed, String collectionName){
-        if (!isConnected){
+    public List<DBObject> findAll(DBObject query, DBObject filed, String collectionName) {
+        if (!isConnected) {
             getConnection();
         }
         List<DBObject> resultList = new ArrayList<DBObject>();
         DBCollection collection = mongoDB.getCollection(collectionName);
         DBCursor cursor = collection.find(query, filed);
-        while (cursor.hasNext()){
+        while (cursor.hasNext()) {
             DBObject dbObject = cursor.next();
             resultList.add(dbObject);
         }
         return resultList;
     }
 
-    public void update(DBObject query, DBObject update, String collectionName){
-        if (!isConnected){
+    public void update(DBObject query, DBObject update, String collectionName) {
+        if (!isConnected) {
             getConnection();
         }
         DBCollection collection = mongoDB.getCollection(collectionName);
         collection.update(query, update);
     }
 
-    public void upsert(DBObject query, DBObject update, boolean multi, String collectionName){
-        if (!isConnected){
+    public void upsert(DBObject query, DBObject update, boolean multi, String collectionName) {
+        if (!isConnected) {
             getConnection();
         }
         DBCollection collection = mongoDB.getCollection(collectionName);
         collection.update(query, update, true, multi);
     }
 
-    public void remove(DBObject query, String collectionName){
-        if (!isConnected){
+    public void remove(DBObject query, String collectionName) {
+        if (!isConnected) {
             getConnection();
         }
         DBCollection collection = mongoDB.getCollection(collectionName);
         collection.remove(query);
     }
 
-    public void removeAll(String collectionName){
-        if (!isConnected){
+    public void removeAll(String collectionName) {
+        if (!isConnected) {
             getConnection();
         }
         DBCollection collection = mongoDB.getCollection(collectionName);
     }
 
-    public void drop(String collectionName){
-        if (!isConnected){
+    public void drop(String collectionName) {
+        if (!isConnected) {
             getConnection();
         }
         DBCollection collection = mongoDB.getCollection(collectionName);
@@ -164,24 +166,24 @@ public class MongoDBUtil {
     }
 
 
-    public int getCount(DBObject query, String collectionName){
-        if (!isConnected){
+    public int getCount(DBObject query, String collectionName) {
+        if (!isConnected) {
             getConnection();
         }
         DBCollection collection = mongoDB.getCollection(collectionName);
-        return (int)collection.count(query);
+        return (int) collection.count(query);
     }
 
-    public int getCollectionSize(String collectionName){
-        if (isConnected){
+    public int getCollectionSize(String collectionName) {
+        if (isConnected) {
             getConnection();
         }
         DBCollection collection = mongoDB.getCollection(collectionName);
         return collection.find().size();
     }
 
-    public DBCollection getCollection(String collectionName){
-        if (!isConnected){
+    public DBCollection getCollection(String collectionName) {
+        if (!isConnected) {
             getConnection();
         }
         return mongoDB.getCollection(collectionName);
