cbda6784ed27ed2396c65ac4ca57151a243964fd
JLeo <ggyyleo@gmail.com>
2013-01-18 18:20:16 +0800

    add date

--- Laputa/src/main/java/BetMatchProcessing/BetMatchBatchProcessorSpecifiedDate.java
+++ Laputa/src/main/java/BetMatchProcessing/BetMatchBatchProcessorSpecifiedDate.java
@@ -126,6 +126,7 @@ public class BetMatchBatchProcessorSpecifiedDate {
         long t2 = System.currentTimeMillis();
 
         System.out.println("\n****\nTotal Match: " + matchList.size() + "\nBet on match: " + BetOnMatch[0] + "\ntotal time:" + (t2 - t1));
+        executorService.shutdown();
     }
 
     private double getHandicap(double type, int abFlag) {
