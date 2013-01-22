3b5e64cd939e6ca7a8452b59f9dead7b872cf411
JLeo <ggyyleo@gmail.com>
2013-01-23 07:36:21 +0800

    port from arg

---
+++ Laputa/src/main/java/BasicDataProcessing/MyBasicDBObject.java
@@ -0,0 +1,14 @@
+package BasicDataProcessing;
+
+import com.mongodb.BasicDBObject;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jleo
+ * Date: 13-1-23
+ * Time: 上午7:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyBasicDBObject extends BasicDBObject {
+
+}
--- football/src/main/groovy/org/Lab.groovy
+++ football/src/main/groovy/org/Lab.groovy
@@ -1,52 +1,26 @@
-import BasicDataProcessing.BasicData
-import Util.MongoDBUtil
 import com.mongodb.BasicDBObject
-import com.mongodb.DBObject
-import org.codehaus.jackson.map.ObjectMapper
 
-def m = MongoDBUtil.getInstance("58.215.168.166","15000","fb")
-c = m.getCollection("obj")
-
-//c.save(new BasicDBObject("aa",new BasicData()))
-
-def bd = new BasicData()
-bd.lose1Game=1.2
-
-ObjectMapper mapper = new ObjectMapper();
-try {
-    String basicDataJson = mapper.writeValueAsString(bd);
-
-    BasicDBObject cacheQuery = new BasicDBObject();
-    cacheQuery.append("win", 1);
-    cacheQuery.append("push", 2);
-    cacheQuery.append("lose", 3);
-    cacheQuery.append("winFactor", 4);
-    cacheQuery.append("pushFactor", 5);
-    cacheQuery.append("loseFactor", 6);
-
-    cacheQuery.append("basicDataJson", basicDataJson);
-    m.insert(cacheQuery, "resultcache");
-} catch (IOException e) {
-    e.printStackTrace();
-}
+win = 1
+push = 2
+lose =3
+winFactor = 4
+pushFactor = 5
+loseFactor = 6
 
 BasicDBObject cacheQuery = new BasicDBObject();
-cacheQuery.append("win", 1);
-cacheQuery.append("push", 2);
-cacheQuery.append("lose", 3);
-cacheQuery.append("winFactor", 4);
-cacheQuery.append("pushFactor", 5);
-cacheQuery.append("loseFactor", 6);
+cacheQuery.append("win", win);
+cacheQuery.append("push", push);
+cacheQuery.append("lose", lose);
+cacheQuery.append("winFactor", winFactor);
+cacheQuery.append("pushFactor", pushFactor);
+cacheQuery.append("loseFactor", loseFactor);
 
-DBObject cached = m.findOne(cacheQuery, "resultcache");
-if (cached != null) {
-    String basicDataJson = (String) cached.get("basicDataJson");
-    try {
-        basicData = mapper.readValue(basicDataJson, BasicData.class);
-        println basicData
-        return;
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
+BasicDBObject cacheQuery2 = new BasicDBObject();
+cacheQuery.append("win", win);
+cacheQuery.append("push", push);
+cacheQuery.append("lose", lose);
+cacheQuery.append("winFactor", winFactor);
+cacheQuery.append("pushFactor", pushFactor);
+cacheQuery.append("loseFactor", loseFactor);
 
-}
+println cacheQuery.toString()
