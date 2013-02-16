4adadd7bb5945017ef6455f660e3e6b4de121f1f
JLeo <ggyyleo@gmail.com>
2013-02-16 10:46:23 +0800

    as

--- football/src/main/groovy/org/basketball/BPNNTrainer.groovy
+++ football/src/main/groovy/org/basketball/BPNNTrainer.groovy
@@ -1,11 +1,7 @@
 package org.basketball
-
 import Util.MongoDBUtil
 import com.mongodb.BasicDBList
 import com.mongodb.BasicDBObject
-
-import java.util.concurrent.atomic.AtomicInteger
-
 /**
  * Created with IntelliJ IDEA.
  * User: jleo
@@ -22,12 +18,12 @@ class BPNNTrainer {
     public static void main(String[] args) throws IOException {
         def numOfFeature = Sharding.getKeyEventAbbr().values().asList()[0..12].size() * 2
 
-        BP bp = new BP(numOfFeature, 10000, 28);
+        BP bp = new BP(numOfFeature, 10, 28);
 
 
         int number = 0;
         MongoDBUtil mongoDBUtil = MongoDBUtil.getInstance(args[0], args[1], "bb");
-        100.times {
+        1.times {
             mongoDBUtil.findAllCursor((["\$or": [["ae": ["\$exists": true] as BasicDBObject] as BasicDBObject, ["be": ["\$exists": true] as BasicDBObject] as BasicDBObject] as BasicDBList] as BasicDBObject).append("url", ['\$lte': '200103300CLE'] as BasicDBObject).append("sec", 720), null, "log").each {
                 if (!it.get("total"))
                     return
@@ -74,62 +70,62 @@ class BPNNTrainer {
         System.out.println("训练完毕");
 
 //        def tests = mongoDBUtil.findAllCursor((["\$or": [["ae": ["\$exists": true] as BasicDBObject] as BasicDBObject, ["be": ["\$exists": true] as BasicDBObject] as BasicDBObject] as BasicDBList] as BasicDBObject).append("url", ['\$gt': '201210300CLE'] as BasicDBObject).append("sec", 720), null, "log")
-        def tests = mongoDBUtil.findAllCursor((["\$or": [["ae": ["\$exists": true] as BasicDBObject] as BasicDBObject, ["be": ["\$exists": true] as BasicDBObject] as BasicDBObject] as BasicDBList] as BasicDBObject).append("url", ['\$lte': '200103300CLE'] as BasicDBObject).append("sec", 720), null, "log")
-        tests = tests.sort(new BasicDBObject(url: 1).append("sec", -1))
-        def count = tests.count()
-        def hit = new AtomicInteger()
-        tests.each {
-            double[] stats = new double[numOfFeature];
-            int index = 0
-            Sharding.keyEventAbbr.values().asList()[0..12].each { abbr ->
-                def countA = it.get("ae").get(abbr)
-                if (countA) {
-                    countA = countA as int
-                    stats[index] += countA
-                } else {
-                    stats[index] = 0
-                }
-                index++
-                def countB = it.get("be").get(abbr)
-                if (countB) {
-                    countB = countB as int
-                    stats[index] += countB
-                } else {
-                    stats[index] = 0
-                }
-                index++
-            }
-//            stats[index] = (it.get("sec") as int) / 10
-
-            def scores = it.get("score").split("-")
-            int current = (scores[0] as int) + (scores[1] as int)
-//            stats[index] = current
+////        def tests = mongoDBUtil.findAllCursor((["\$or": [["ae": ["\$exists": true] as BasicDBObject] as BasicDBObject, ["be": ["\$exists": true] as BasicDBObject] as BasicDBObject] as BasicDBList] as BasicDBObject).append("url", ['\$lte': '200103300CLE'] as BasicDBObject).append("sec", 720), null, "log")
+//        tests = tests.sort(new BasicDBObject(url: 1).append("sec", -1))
+//        def count = tests.count()
+//        def hit = new AtomicInteger()
+//        tests.each {
+//            double[] stats = new double[numOfFeature];
+//            int index = 0
+//            Sharding.keyEventAbbr.values().asList()[0..12].each { abbr ->
+//                def countA = it.get("ae").get(abbr)
+//                if (countA) {
+//                    countA = countA as int
+//                    stats[index] += countA
+//                } else {
+//                    stats[index] = 0
+//                }
+//                index++
+//                def countB = it.get("be").get(abbr)
+//                if (countB) {
+//                    countB = countB as int
+//                    stats[index] += countB
+//                } else {
+//                    stats[index] = 0
+//                }
+//                index++
+//            }
+////            stats[index] = (it.get("sec") as int) / 10
 //
-//            index++
-
-            double[] result = bp.test(stats)
-
-            double max = -1;
-            int maxIndex = 0;
-            for (int i = 0; i < result.length; i++) {
-                double c = result[i];
-                if (c > max) {
-                    max = c
-                    maxIndex = i;
-                }
-            }
-            println it.get("url")
-            println it.get("sec")
-            int total = it.get("total")
-            println maxIndex
-            println "result:" + ((maxIndex * 3) + current) + ", actual:" + total
-
-            if (maxIndex * 5 + 100 < total && maxIndex * 5 + 105 > total) {
-                hit++
-            }
-        }
-
-        println hit / count * 100
+//            def scores = it.get("score").split("-")
+//            int current = (scores[0] as int) + (scores[1] as int)
+////            stats[index] = current
+////
+////            index++
+//
+//            double[] result = bp.test(stats)
+//
+//            double max = -1;
+//            int maxIndex = 0;
+//            for (int i = 0; i < result.length; i++) {
+//                double c = result[i];
+//                if (c > max) {
+//                    max = c
+//                    maxIndex = i;
+//                }
+//            }
+//            println it.get("url")
+//            println it.get("sec")
+//            int total = it.get("total")
+//            println maxIndex
+//            println "result:" + ((maxIndex * 3) + current) + ", actual:" + total
+//
+//            if (maxIndex * 5 + 100 < total && maxIndex * 5 + 105 > total) {
+//                hit++
+//            }
+//        }
+//
+//        println hit / count * 100
     }
 
 }
---
+++ football/src/main/groovy/org/basketball/Joone.groovy
@@ -0,0 +1,245 @@
+package org.basketball
+
+import Util.MongoDBUtil
+import com.mongodb.BasicDBList
+import com.mongodb.BasicDBObject
+import org.joone.engine.*
+import org.joone.engine.learning.TeachingSynapse
+import org.joone.io.MemoryInputSynapse
+import org.joone.io.MemoryOutputSynapse
+import org.joone.net.NeuralNet
+
+public class Joone implements NeuralNetListener, Serializable {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 61750666816215273L;
+    private NeuralNet nnet = null;
+    private MemoryInputSynapse inputSynapse, desiredOutputSynapse;
+    private MemoryOutputSynapse outputSynapse;
+
+    protected void initNeuralNet() {
+        // First create the three layers
+        LinearLayer input = new LinearLayer();
+        SigmoidLayer hidden = new SigmoidLayer();
+        SigmoidLayer output = new SigmoidLayer();
+        input.setLayerName("input");
+        hidden.setLayerName("hidden");
+        output.setLayerName("output");
+        // set the dimensions of the layers
+
+        def numOfFeature = Sharding.getKeyEventAbbr().values().asList()[0..12].size() * 2
+        input.setRows(numOfFeature);
+        hidden.setRows(20);
+        output.setRows(28);
+
+        // Now create the two Synapses
+        FullSynapse synapse_IH = new FullSynapse(); /* input -> hidden conn. */
+        FullSynapse synapse_HO = new FullSynapse(); /* hidden -> output conn. */
+        // Connect the input layer whit the hidden layer
+        input.addOutputSynapse(synapse_IH);
+        hidden.addInputSynapse(synapse_IH);
+        // Connect the hidden layer whit the output layer
+        hidden.addOutputSynapse(synapse_HO);
+        output.addInputSynapse(synapse_HO);
+        // the input to the neural net
+        inputSynapse = new MemoryInputSynapse();
+        input.addInputSynapse(inputSynapse);
+        // the output of the neural net
+        outputSynapse = new MemoryOutputSynapse();
+        output.addOutputSynapse(outputSynapse);
+        // The Trainer and its desired output
+        desiredOutputSynapse = new MemoryInputSynapse();
+        TeachingSynapse trainer = new TeachingSynapse();
+        trainer.setDesired(desiredOutputSynapse);
+        nnet = new NeuralNet();
+        nnet.addLayer(input, NeuralNet.INPUT_LAYER);
+        10.times {
+            nnet.addLayer(hidden, NeuralNet.HIDDEN_LAYER);
+        }
+        nnet.addLayer(output, NeuralNet.OUTPUT_LAYER);
+        nnet.setTeacher(trainer);
+        output.addOutputSynapse(trainer);
+//      File
+    }
+
+    public int test(double[][] d) {
+        inputSynapse.setInputArray(d);
+        inputSynapse.setAdvancedColumnSelector("1,2,3,4,5,6,7,8,9,10,11,12,13");
+        nnet.getMonitor().setTotCicles(1);
+        nnet.start();
+        nnet.getMonitor().Go();
+        for (Object o : outputSynapse.getAllPatterns()) {
+            Pattern p = (Pattern) o;
+
+            double max = -1;
+            int maxIndex = 0;
+            for (int i = 0; i < p.getArray().length; i++) {
+                double c = p.getArray()[i];
+                if (c > max) {
+                    max = c
+                    maxIndex = i;
+                }
+            }
+            return maxIndex;
+        }
+    }
+
+    public void train(double[][] inputArray, double[][] desiredOutputArray) {
+        // set the inputs
+        inputSynapse.setInputArray(inputArray);
+        inputSynapse.setInputFull(true)
+        inputSynapse.setAdvancedColumnSelector("1,2,3,4,5,6,7,8,9,10,11,12,13");
+        // set the desired outputs
+        desiredOutputSynapse.setInputArray(desiredOutputArray);
+        desiredOutputSynapse.setAdvancedColumnSelector("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28");
+        // get the monitor object to train or feed forward
+        Monitor monitor = nnet.getMonitor();
+        // set the monitor parameters
+        monitor.setLearningRate(0.8);
+        monitor.setMomentum(0.3);
+        monitor.setTrainingPatterns(inputArray.length);
+        monitor.setTotCicles(20);
+        monitor.setLearning(true);
+        nnet.addNeuralNetListener(this);
+        nnet.start();
+        nnet.getMonitor().Go();
+        for (Object o : outputSynapse.getAllPatterns()) {
+            Pattern p = (Pattern) o;
+            System.out.println(10 * p.getArray()[0]);
+        }
+    }
+
+    public void cicleTerminated(NeuralNetEvent e) {
+
+    }
+
+    public void errorChanged(NeuralNetEvent e) {
+    }
+
+    public void netStarted(NeuralNetEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+    public void netStopped(NeuralNetEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+    public void netStoppedError(NeuralNetEvent e, String error) {
+    }
+
+    public static void main(String[] args) {
+        MongoDBUtil mongoDBUtil = MongoDBUtil.getInstance(args[0], args[1], "bb");
+
+        Joone joone = new Joone();
+        joone.initNeuralNet();
+
+        def cursor = mongoDBUtil.findAllCursor((["\$or": [["ae": ["\$exists": true] as BasicDBObject] as BasicDBObject, ["be": ["\$exists": true] as BasicDBObject] as BasicDBObject] as BasicDBList] as BasicDBObject).append("url", ['\$lte': '200103300CLE'] as BasicDBObject).append("sec", 720), null, "log")
+        def allTraining = new double[cursor.count()][26*2]
+        def allReal = new double[cursor.count()][28]
+        int number = 0
+        cursor.each {
+            if (!it.get("total"))
+                return
+
+            double[] stats = new double[26*2];
+            int index = 0
+            Sharding.keyEventAbbr.values().asList()[0..12].each { abbr ->
+                def countA = it.get("ae").get(abbr)
+//                println abbr
+//                println countA
+
+                if (countA) {
+                    countA = countA as int
+
+                    if (abbr == "or")
+                        println countA
+                    if (countA > Sharding.divide.get(abbr)) {
+                        stats[index] = 1
+                    }
+                } else {
+                    stats[index] = 0
+                }
+                index += 2
+                def countB = it.get("be").get(abbr)
+                if (countB) {
+                    countB = countB as int
+                    if (countB > Sharding.divide.get(abbr)) {
+                        stats[index] = 1
+                    }
+                } else {
+                    stats[index] = 0
+                }
+                index += 2
+            }
+//            stats[index] = (it.get("sec") as int) / 10
+
+            def scores = it.get("score").split("-")
+            int current = (scores[0] as int) + (scores[1] as int)
+//            stats[index] = current
+
+//            index++
+
+            int result = (it.get("total") - current) / 3
+//            println result
+            double[] real = new double[28];
+            real[result] = 1
+
+            allTraining[number] = stats
+            allReal[number] = real
+//            bp.train(stats, real);
+            number++
+
+            if (number % 1000 == 0)
+                println number
+        }
+//        joone.train(allTraining, allReal);
+//
+//        def tests = mongoDBUtil.findAllCursor((["\$or": [["ae": ["\$exists": true] as BasicDBObject] as BasicDBObject, ["be": ["\$exists": true] as BasicDBObject] as BasicDBObject] as BasicDBList] as BasicDBObject).append("url", ['\$gt': '201210300CLE'] as BasicDBObject).append("sec", 720), null, "log")
+////        def tests = mongoDBUtil.findAllCursor((["\$or": [["ae": ["\$exists": true] as BasicDBObject] as BasicDBObject, ["be": ["\$exists": true] as BasicDBObject] as BasicDBObject] as BasicDBList] as BasicDBObject).append("url", ['\$lte': '200103300CLE'] as BasicDBObject).append("sec", 720), null, "log")
+//        tests = tests.sort(new BasicDBObject(url: 1).append("sec", -1))
+//        def count = tests.count()
+//        def hit = new AtomicInteger()
+//        tests.each {
+//            double[][] stats = new double[1][26*2];
+//            int index = 0
+//            Sharding.keyEventAbbr.values().asList()[0..12].each { abbr ->
+//                def countA = it.get("ae").get(abbr)
+//                if (countA) {
+//                    countA = countA as int
+//                    stats[0][index] += countA
+//                } else {
+//                    stats[0][index] = 0
+//                }
+//                index++
+//                def countB = it.get("be").get(abbr)
+//                if (countB) {
+//                    countB = countB as int
+//                    stats[0][index] += countB
+//                } else {
+//                    stats[0][index] = 0
+//                }
+//                index++
+//            }
+////            stats[index] = (it.get("sec") as int) / 10
+//
+//            def scores = it.get("score").split("-")
+//            int current = (scores[0] as int) + (scores[1] as int)
+////            stats[index] = current
+////
+////            index++
+//
+//            int pos = joone.test(stats)
+//
+//            println it.get("url")
+//            println it.get("sec")
+//            int total = it.get("total")
+//            println pos
+//            println "result:" + ((pos * 3) + current) + ", actual:" + total
+
+//            result = result[0]
+//
+
+//        }
+    }
+}
