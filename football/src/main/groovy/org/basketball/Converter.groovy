748cc2349fffeba37b6a84affb04f6b47d1345ce
JLeo <ggyyleo@gmail.com>
2013-02-17 15:10:01 +0800

    add logs

--- football/src/main/groovy/org/basketball/Converter.groovy
+++ football/src/main/groovy/org/basketball/Converter.groovy
@@ -40,10 +40,15 @@ class Converter {
                     while (true) {
                         def task = tasks.poll(30, TimeUnit.SECONDS)
                         println task
-                        updateQuarter(task, 2160, 2880, 1)
-                        updateQuarter(task, 1440, 2160, 2)
-                        updateQuarter(task, 720, 1440, 3)
-                        updateQuarter(task, 0, 720, 4)
+
+                        try {
+                            updateQuarter(task, 2160, 2880, 1)
+                            updateQuarter(task, 1440, 2160, 2)
+                            updateQuarter(task, 720, 1440, 3)
+                            updateQuarter(task, 0, 720, 4)
+                        } catch (e) {
+                            e.printStackTrace()
+                        }
                     }
                 }
             })
@@ -71,7 +76,7 @@ class Converter {
                 scoreB += diffB as int
             }
         }
-
-        mongoDBUtil.update([url: line, sec: to] as BasicDBObject, ['\$set': ["q${quarter}a": scoreA, "q${quarter}b": scoreB]] as BasicDBObject, "log", true)
+        mongoDBUtil.insert([url:line, quarter:quarter, scoreA:scoreA, scoreB:scoreB] as BasicDBObject, "log")
+        //mongoDBUtil.update([url: line, sec: to] as BasicDBObject, ['\$set': ["q${quarter}a": scoreA, "q${quarter}b": scoreB]] as BasicDBObject, "log", true)
     }
 }
