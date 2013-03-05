97813fb597e6f9d137fee2568922bdcaf8547def
JLeo <ggyyleo@gmail.com>
2013-03-05 11:14:28 +0800

    add save rawInfo

--- rm/src/main/groovy/org/basketball/Lab.groovy
+++ rm/src/main/groovy/org/basketball/Lab.groovy
@@ -8,26 +8,27 @@ package org.basketball
  * Let's RocknRoll
  */
 
-//def pptv = new File("/Users/jleo/Downloads/Aegis PPTV0228.csv")
-//def sdk = new File("/Users/jleo/Downloads/sdk_11003_12334.csv")
-//
-//def sdkMap = [:]
-//sdk.eachLine { sdkLine ->
-//    def split = sdkLine.split(",")
-//    if (split.size() >= 7)
-//        sdkMap.put(split[0], split[6..-1].join(";"))
-//}
-//
-//def file = new File("/Users/jleo/Downloads/output.csv")
-//file.createNewFile()
-//pptv.eachLine { line ->
-//    if (line) {
-//        def cookie = line.split(";")[2]
-//
-//        String toAppend = sdkMap.get(cookie.substring(1,cookie.length()-1))
-//        if (toAppend)
-//            file.append(cookie + ";" + toAppend+"\n")
-//    }
-//}
+def pptv = new File("/Users/jleo/Downloads/Aegis PPTV0304.csv")
+def sdk = new File("/Users/jleo/Downloads/sdk_11055_12334_0304.csv")
 
-println ([*(0..25)].subsequences())
+def sdkMap = [:]
+sdk.eachLine { sdkLine ->
+    def split = sdkLine.split(",")
+    if (split.size() >= 7)
+        sdkMap.put(split[0], split[6..-1].join(";"))
+}
+
+def file = new File("/Users/jleo/Downloads/output.csv")
+file.createNewFile()
+pptv.eachLine { line ->
+    if (line) {
+        def split = line.split(";")
+        def cookie = split[2]
+        def id = split[0]
+        String toAppend = sdkMap.get(cookie.substring(1, cookie.length() - 1))
+        if (toAppend)
+            file.append(cookie + ";" + toAppend + ";" + id + "\n")
+    }
+}
+
+//println ([*(0..25)].subsequences())
