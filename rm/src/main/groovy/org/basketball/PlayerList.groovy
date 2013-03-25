c3c7bbd7837b102c10b1853a4f0274dd24393aa9
JLeo <ggyyleo@gmail.com>
2013-03-25 21:17:45 +0800

    add save rawInfo

---
+++ rm/src/main/groovy/org/basketball/PlayerList.groovy
@@ -0,0 +1,18 @@
+package org.basketball
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jleo
+ * Date: 13-3-25
+ * Time: 下午8:06
+ * Let's RocknRoll
+ */
+
+
+def file = new File("/Users/jleo/players.txt")
+('a'..'z').each { letter ->
+    def url = new URL("http://www.basketball-reference.com/players/" + letter + "/")
+    url.text.eachMatch("href=\"/players/.*?.html") {
+        file.append("http://www.basketball-reference.com" + it[6..-1] + "\n")
+    }
+}
